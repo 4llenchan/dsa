e7b7cce22808d4750547743a3f05811ae55cb713
Allen Chan <allenchan1010@outlook.com>
2021-09-12 15:53:23 +0800

    Implement OF number 46

---
+++ of/OF46.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int translateNum(int num) {
+        vector<int> nums;
+        while (num != 0) {
+            nums.insert(nums.begin(), num % 10);
+            num /= 10;
+        }
+        int n = nums.size();
+        int a = 1;
+        int b = 1;
+        int tmp;
+        for (int i = 1; i < n; ++i) {
+            tmp = nums[i - 1] * 10 + nums[i];
+            int c;
+            if (tmp <= 25 && tmp >= 10) {
+                c = a + b;
+            } else {
+                c = b;
+            }
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
+
+class OF46Tests : public ::testing::Test {
+protected:
+    Solution solution;
+};
+
+TEST_F(OF46Tests, normal) {
+    EXPECT_EQ(5, solution.translateNum(12258));
+    EXPECT_EQ(9, solution.translateNum(123123));
+}
