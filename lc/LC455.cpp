3307f5aa431ddec09796769accd2b9089b8b2727
Allen Chan <allenchan1010@outlook.com>
2021-10-08 17:41:32 +0800

    Implement LC number 455

---
+++ lc/LC455.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+#include "Common.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int gIndex = 0;
+        int sIndex = 0;
+        while (gIndex < g.size() && sIndex < s.size()) {
+            if (g[gIndex] <= s[sIndex]) {
+                ++gIndex;
+            }
+            ++sIndex;
+        }
+        return gIndex;
+    }
+};
+
+class LC455Tests : public ::testing::Test {
+protected:
+    Solution solution;
+};
+
+TEST_F(LC455Tests, normal) {
+    vector<int> g{1, 2, 3};
+    vector<int> s{1, 1};
+    EXPECT_EQ(solution.findContentChildren(g, s), 1);
+}
+
+TEST_F(LC455Tests, normal1) {
+    vector<int> g{1, 2};
+    vector<int> s{1, 2, 3};
+    EXPECT_EQ(solution.findContentChildren(g, s), 2);
+}
